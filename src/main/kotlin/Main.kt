6115247e389df2fae3ddb6103fd6c303df80b3ac
Maximov Nikolay <n.d.maximov@gmail.com>
2023-11-11 14:50:11 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val numberOrOrders: Int = 42
+    var notificationText: String = "Не забудьте маску и перчатки!"
+
+    println(numberOrOrders)
+    println(notificationText)
+
+    notificationText = "Маски больше не требуются!"
+
+    println(notificationText)
+}
